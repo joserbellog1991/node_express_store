8104bce3fe64e97060b4905ad7edbbaa438fe916
Jose Bello <joserbellog1991@gmail.com>
2023-03-29 10:17:37 -0300

    actualizacion del index

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const routerApi = require("./routes/index");
 const {errorHandler, logErrors,boomErrorHandler} = require("./middlewares/error.handler");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-
+const whitelist = ['http://localhost:3000', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
 
 app.use(express.json());
-
+//app.use(cors(options));
+app.use(cors());
 routerApi(app);
 
 app.use(logErrors);
